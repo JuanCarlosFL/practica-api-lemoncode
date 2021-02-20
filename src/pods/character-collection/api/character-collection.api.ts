d0bdac83799cafc1fcd59326ad6373c01aeaf8d0
JuanCarlosFL <jcfmalaga@gmail.com>
2021-02-20 19:01:13 +0100

    fetching to api/results

--- src/pods/character-collection/api/character-collection.api.ts
+++ src/pods/character-collection/api/character-collection.api.ts
@@ -1,7 +1,10 @@
 import { CharacterEntityApi } from './character-collection.api.model';
 
+const url = 'api/results';
+
 export const getCharacterCollection = async (): Promise<CharacterEntityApi[]> => {
-  const response = await fetch('https://rickandmortyapi.com/api/character');
-      const data = await response.json()
-      return data.results;
+  const response = await fetch(url);
+  const data = await response.json()
+  console.log(data)
+      return data;
 }
