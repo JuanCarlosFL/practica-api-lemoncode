0a9cdaaa8b1fe0507e18d95bb75f26151df778c1
JuanCarlosFL <jcfmalaga@gmail.com>
2021-02-21 09:12:53 +0100

    working on handling errors

--- src/pods/character-collection/api/character-collection.api.ts
+++ src/pods/character-collection/api/character-collection.api.ts
@@ -1,7 +1,12 @@
 import { CharacterEntityApi } from './character-collection.api.model';
 
 export const getCharacterCollection = async (): Promise<CharacterEntityApi[]> => {
-  const response = await fetch('https://rickandmortyapi.com/api/character');
-      const data = await response.json()
-      return data.results;
+  try {
+    const response = await fetch('https://rickandmortyapi.com/api/character');
+        const data = await response.json()
+        return data.results;
+
+  } catch (err) {
+
+  }
 }
